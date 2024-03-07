1e7c82ce050b9b31798b1947648d565251ad1571
Frederick.Laroche <139751013+brightfred@users.noreply.github.com>
2024-03-07 22:38:20 +1300

    basic console log and constant ```

---
+++ src/number.js
@@ -0,0 +1,39 @@
+//NOTE - i declare a variable with const with the value of 10, a variable with let with the value of 20 and a variable with var with the value of 30. 
+//NOTE - i will use these variables in the index.js file to show the difference between const, let and var.
+//NOTE - = is to assign a value to a variable.
+
+// console.log('num1')
+
+
+const num1 = 10
+console.log(num1)
+let num2 = 20
+console.log(num2)
+var num3 = 30
+console.log(num3)
+
+
+const firstName = 'Ali'
+let lastName = 'Khuwaji'
+console.log(firstName + ' ' + lastName)
+
+const expression = 'I am a impressed by'
+console.log(expression + ' ' + firstName + ' ' + lastName)
+
+const meme = 'what are you doing man !?!'
+console.log(meme)
+const tutors = ''
+
+
+
+/*
+
+if(conditon here){
+    do something{}
+else
+    do something else{}
+}
+
+*/
+
+
